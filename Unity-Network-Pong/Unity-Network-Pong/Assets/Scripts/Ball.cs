27e675cdfab43b6004982687439580589c4d4002
ej84 <54951468+ej84@users.noreply.github.com>
2021-06-26 21:33:38 -0400

    added

---
+++ Unity-Network-Pong/Unity-Network-Pong/Assets/Scripts/Ball.cs
@@ -0,0 +1,16 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class Ball : MonoBehaviourPun
+{
+    public bool IsMasterClientLocal => PhotonNetwork.IsMasterClient && photonView.IsMine;
+
+    private Vector2 direction = Vector2.right;
+    private readonly float speed = 10f;
+    private readonly float randomRefectionIntensity = 0.1f;
+    
+    private void FixedUpdate()
+    {
+
+    }
+}
---
+++ Unity-Network-Pong/Unity-Network-Pong/Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null) instance = FindObjectOfType<GameManager>();
+
+            return instance;
+        }
+    }
+
+    private static GameManager instance;
+
+    public Text scoreText;
+    public Transform[] spawnPositions;
+    public GameObject playerPrefab;
+    public GameObject ballPrefab;
+
+    private int[] playerScores;
+
+    private void Start()
+    {
+
+    }
+
+    private void SpawnPlayer()
+    {
+
+    }
+
+    private void SpawnBall()
+    {
+
+    }
+
+    public override void OnLeftRoom()
+    {
+
+    }
+
+    public void AddScore(int playerNumber, int score)
+    {
+
+    }
+
+    
+    [PunRPC]
+    private void RPCUpdateScoreText(string player1ScoreText, string player2ScoreText)
+    {
+
+    }
+}
