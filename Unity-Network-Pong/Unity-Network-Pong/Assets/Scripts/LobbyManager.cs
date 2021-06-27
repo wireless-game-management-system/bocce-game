71b06c1bed700d980168a26e235b621a58871ce3
ej84 <54951468+ej84@users.noreply.github.com>
2021-06-26 21:32:21 -0400

    added

---
+++ Unity-Network-Pong/Unity-Network-Pong/Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,67 @@
+﻿    using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    private readonly string gameVersion = "1";
+
+    public Text connectionInfoText;
+    public Button joinButton;
+    
+    private void Start()
+    {
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+
+        joinButton.interactable = false;
+        connectionInfoText.text = "Connecting To Master Server...";
+    }
+    
+    public override void OnConnectedToMaster()
+    {
+        joinButton.interactable = true;
+        connectionInfoText.text = "Online : Connected to Master Server.";
+    }
+    
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        joinButton.interactable = false;
+        connectionInfoText.text = $"Offline : Connection Disabled {cause.ToString()} - Try reconnecting..";
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    
+    public void Connect()
+    {
+        joinButton.interactable = false;
+
+        if (PhotonNetwork.IsConnected)
+        {
+            connectionInfoText.text = "Connecting to random room...";
+
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+        else
+        {
+            connectionInfoText.text = $"Offline : Connection Disabled - Try to reconnecting..";
+
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+    }
+    
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        connectionInfoText.text = "There is no empty room, Creating new room.";
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 2 });
+
+    }
+    
+    public override void OnJoinedRoom()
+    {
+        connectionInfoText.text = "Connected to with Room.";
+        PhotonNetwork.LoadLevel("Main");
+    }
+}
---
+++ Unity-Network-Pong/Unity-Network-Pong/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class Player : MonoBehaviourPun
+{
+    private Rigidbody2D playerRigidbody;
+    private SpriteRenderer spriteRenderer;
+    
+    public float speed = 3f;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
