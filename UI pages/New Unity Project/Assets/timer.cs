189c3180bf9e15c98da02469a4b554d1c8975336
ej84 <54951468+ej84@users.noreply.github.com>
2021-07-12 11:12:35 -0400

    playing status updated

---
+++ UI pages/New Unity Project/Assets/timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    public float timelimit;
+    public Text text_timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timelimit += Time.deltaTime;
+        text_timer.text = " Time: " + Mathf.Round(timelimit);
+    }
+}
