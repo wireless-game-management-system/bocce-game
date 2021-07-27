2b0712c2c1c003a878e37ad1f26f9a1c1dc22e73
ej84 <54951468+ej84@users.noreply.github.com>
2021-07-26 21:40:11 -0400

    User script updated

--- Assets/scripts/Database/User.cs
+++ Assets/scripts/Database/User.cs
@@ -8,10 +8,17 @@ public class User : MonoBehaviour
 {
     public string userName;
     public int userScore;
+    public string teamName;
+    public string teamMateName;
+    public int ballLocation;
 
     public User()
     {
         userName = DatabaseTest.playerName;
         userScore = DatabaseTest.playerScore;
+        teamName = DatabaseTest.teamName;
+        teamMateName = DatabaseTest.teamMate;
+        ballLocation = DatabaseTest.ballLocation;
+
     }
 }
