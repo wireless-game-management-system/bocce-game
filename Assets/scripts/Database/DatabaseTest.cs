8c300e3277608601b7f90e527384f7b48412c793
ej84 <54951468+ej84@users.noreply.github.com>
2021-07-26 21:21:01 -0400

    database script complete

--- Assets/scripts/Database/DatabaseTest.cs
+++ Assets/scripts/Database/DatabaseTest.cs
@@ -10,23 +10,36 @@ public class DatabaseTest : MonoBehaviour
 
     public Text scoreText;
     public InputField nameText;
+    public Text teamNameText;
+    public InputField teamText;
+    public InputField teamMateText;
+    public InputField ballLocationText;
 
     private System.Random random = new System.Random();
 
     public static int playerScore;
     public static string playerName;
+    public static string teamName;
+    public static string teamMate;
+    public static int ballLocation;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         playerScore = random.Next(0, 100);
-        scoreText.text = "Score: " + playerScore;
+        scoreText.text = "Your Score: " + playerScore;
+        
     }
 
     public void onSubmit() 
     {
         playerName = nameText.text;
+        teamName = teamText.text;
+        teamMate = teamMateText.text;
+        ballLocation = int.Parse(ballLocationText.text);
+
         PostToDatabase();
     }
 
@@ -34,7 +47,9 @@ public class DatabaseTest : MonoBehaviour
 
     {
         User user = new User();
-        RestClient.Put("https://bocce-game-default-rtdb.firebaseio.com/.json", user);
+        RestClient.Post("https://unity-game-767f2-default-rtdb.firebaseio.com/.json", user);
+        
+        
     }
 
 }
